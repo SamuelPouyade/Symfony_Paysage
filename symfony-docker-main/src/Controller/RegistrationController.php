5f0ac3a79eaf3db3213b45864953363a10989448
Samuel <samuel.pouyade@outlook.fr>
2023-10-18 16:59:35 +0200

    Ajout des articles

--- symfony-docker-main/src/Controller/RegistrationController.php
+++ symfony-docker-main/src/Controller/RegistrationController.php
@@ -19,6 +19,7 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new Users();
+        $user->setRoles(['ROLE_USER']);
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
